195ca01621343d8f27482cefdf02b718f3fa7056
Jc Paredes <paredesjancarlo99@gmail.com>
2022-08-15 21:08:03 +0800

    Revert "added search function"
    This reverts commit cdc2975bff5ea281f840da9a8d954f6be2d34129.

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import json
 import time
 import random
 import pyperclip
@@ -10,12 +9,6 @@ def save():
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
-    new_data = {
-        website: {
-            "email": email,
-            "password": password
-        }
-    }
 
     if len(website) == 0 or len(email) == 0 or len(password) == 0:
         messagebox.showwarning(title="Oops! Empty Field", message="Please do not leave any fields empty!")
@@ -25,42 +18,13 @@ def save():
                                                               f"Email: {email}\n"
                                                               f"Password: {password}\n"
                                                               f"Save?")
-        if is_ok:
-            try:
-                with open("data.json", "r") as data_file:
-                    data = json.load(data_file)
-            except FileNotFoundError:
-                with open("data.json", "w") as data_file:
-                    json.dump(new_data, data_file, indent=4)
-            else:
-                data.update(new_data)
 
-                with open("data.json", "w") as data_file:
-                    json.dump(data, data_file, indent=4)
-            finally:
+        if is_ok:
+            with open("data", "a") as data_file:
+                data_file.write(f"{user_time} -  {website} | {email} | {password}\n")
                 website_entry.delete(0, END)
                 email_entry.delete(0, END)
                 password_entry.delete(0, END)
-
-
-def find_password():
-    website = website_entry.get()
-    try:
-        with open("data.json") as data_file:
-            data = json.load(data_file)
-    except FileNotFoundError:
-        messagebox.showwarning(title="Error!", message="No Data File Found.")
-    else:
-        if website in data:
-            email = data[website]["email"]
-            password = data[website]["password"]
-            messagebox.showinfo(title=website, message=f"Email: {email}\nPassword: {password}")
-            email_entry.delete(0, END)
-            password_entry.delete(0, END)
-            email_entry.insert(END, email)
-            password_entry.insert(END, password)
-        else:
-            messagebox.showwarning(title="Error!", message=f"No details for {website} exists.")
 
 
 def generate_password():
@@ -104,19 +68,17 @@ website_label.grid(column=1, row=2)
 email_label.grid(column=1, row=3)
 password_label.grid(column=1, row=4)
 
-website_entry = Entry(width=21)
+website_entry = Entry(width=35)
 email_entry = Entry(width=35)
 password_entry = Entry(width=21)
-website_entry.grid(column=2, row=2)
+website_entry.grid(column=2, row=2, columnspan=2)
 website_entry.focus()
 email_entry.grid(column=2, row=3, columnspan=2)
 email_entry.insert(END, "sample@example.com")
 password_entry.grid(column=2, row=4)
 
-search_button = Button(text="Search", width=13, command=find_password)
 generate_button = Button(text="Generate Password", command=generate_password)
 add_button = Button(text="Add", width=35, command=save)
-search_button.grid(column=3, row=2)
 generate_button.grid(column=3, row=4)
 add_button.grid(column=2, row=5, columnspan=2)
 
